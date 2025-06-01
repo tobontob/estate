ad9331244303d5e899df5769858f42b7faaea9bb
tobontob <tobontob@naver.com>
2025-06-01 23:43:29 +0900

    upload

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        // API 요청을 위한 CORS 설정
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
-  },
-  // 외부 도메인에서의 이미지 로드 허용
-  images: {
-    domains: ['openapi.seoul.go.kr'],
-  },
-};
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
